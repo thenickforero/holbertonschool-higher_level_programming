56c1daefa56b582e25eb84d6c9569c2bfe791064
NickForero11 <nicolasforero1199@gmail.com>
2019-09-10 15:09:00 -0500

    Fixed task 1 (Wrong output in case number == 0)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 last_digit = (-1 * ((number * -1) % 10)) if number < 0 else number % 10
 print('Last digit of {:d} is {:d} and is '.format(number, last_digit), end="")
 if last_digit == 0:
-    print('zero')
+    print('0')
 elif number <= 5:
     print('less than 6 and not 0')
 else:
