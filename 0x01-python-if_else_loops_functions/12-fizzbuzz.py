1d0805ab6dc8605565c19e6123108d9e19863cdf
NickForero11 <nicolasforero1199@gmail.com>
2019-09-10 20:00:44 -0500

    Fixed task 9 and 12 (Wrong output)

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
+    for i in range(1, 101):
         if i % 3 == 0:
-            print("{:s}".format("Fizz"), end=" ")
+            print("{:s}".format("Fizz"), end="")
         if i % 5 == 0:
-            print("{:s}".format("Buzz"), end=" ")
+            print("{:s}".format("Buzz"), end="")
         if i % 5 != 0 and i % 3 != 0:
-            print(i, end=" " if i != 100 else "")
+            print(i, end="")
+        print("{:s}".format(' '), end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        last_digit = (-1 * ((number * -1) % 10))
+        last_digit = ((number * -1) % 10)
     else:
         last_digit = number % 10
+    print("{:d}".format(last_digit), end="")
     return last_digit
