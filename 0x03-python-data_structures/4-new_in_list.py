53ed4ba929e1b98cf45f89d5565e18d46bfe08ea
NickForero11 <nicolasforero1199@gmail.com>
2019-09-18 00:40:17 -0500

    Submit the task 4 and removed test file.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    response = my_list[:]
+    response = my_list.copy()
     if int(idx) < 0 or int(idx) > len(my_list):
         return response
     response[idx] = element
