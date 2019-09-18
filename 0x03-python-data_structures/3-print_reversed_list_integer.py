0bf1ddfdd552b758035a42807bf72e9c4b8a2187
NickForero11 <nicolasforero1199@gmail.com>
2019-09-18 00:43:58 -0500

    Fixed task 3(wrong output case empty input) and task 4(wrong index checking).

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        for item in reversed(my_list):
-            print('{:d}'.format(item))
+		return None
+    for item in reversed(my_list):
+        print('{:d}'.format(item))
