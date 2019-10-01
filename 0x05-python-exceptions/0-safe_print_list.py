a767c1fdea060bbdf8c9d35bbe118b2ad8466bcd
NickForero11 <nicolasforero1199@gmail.com>
2019-10-01 00:29:11 -0500

    Fixed task 0 (better counter implementation).

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    failed = False
+    counter = 0
     for i in range(int(x)):
         try:
             print("{}".format(my_list[i]), end="")
+            counter += 1
         except IndexError:
-            failed = True
-            break
+            pass
     print()
-    return i + 1 if not failed else i
+    return counter
