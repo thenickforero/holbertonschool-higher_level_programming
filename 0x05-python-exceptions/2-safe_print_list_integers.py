6f3e492bda8cf1951507cd62b53127a0a0785d7c
NickForero11 <nicolasforero1199@gmail.com>
2019-10-01 00:37:45 -0500

    Fixed task 2 (unnecesary cast and deleted else from try except by requirement).

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     num_elements_printed = 0
     for index in range(int(x)):
         try:
-            data = int(my_list[index])
+            print("{}".format(my_list[index]), end="")
+            num_elements_printed += 1
         except (ValueError, TypeError):
             pass
-        else:
-            print("{}".format(data), end="")
-            num_elements_printed += 1
     print()
     return num_elements_printed
