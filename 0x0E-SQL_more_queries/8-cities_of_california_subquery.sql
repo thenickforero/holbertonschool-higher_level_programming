2d52e4f8e381b83a26f1f8812910d6a6889d8d38
NickForero11 <nicolasforero1199@gmail.com>
2019-11-27 01:31:45 -0500

    Added functions using join's and subquery's

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- Script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT id, name
+FROM cities
+WHERE state_id =
+(
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+-- Results must be sorted in ascending order by cities.id
+ORDER BY cities.id ASC;
