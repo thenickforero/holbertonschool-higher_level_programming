88d9dba0acf5965e6d1f369fc31acb1f7d512666
NickForero11 <nicolasforero1199@gmail.com>
2020-01-10 00:16:20 -0500

    Changed function to use binary search and base conditions

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,4 +17,21 @@ def find_peak(list_of_integers):
                if the list is empty it returns None.
     """
 
-    return max(list_of_integers) if list_of_integers else None
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    middle = int(len(list_of_integers) / 2)
+
+    if (list_of_integers[middle] > list_of_integers[middle - 1] and
+            list_of_integers[middle] > list_of_integers[middle + 1]):
+        return list_of_integers[middle]
+
+    if list_of_integers[middle + 1] > list_of_integers[middle]:
+        return find_peak(list_of_integers[middle + 1:])
+
+    return find_peak(list_of_integers[0:middle])
