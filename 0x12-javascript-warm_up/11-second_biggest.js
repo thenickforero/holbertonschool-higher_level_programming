ee9d196a7b6db599589a2fe89498ed5c645f37e2
NickForero11 <nicolasforero1199@gmail.com>
2020-02-05 02:21:09 -0500

    Fixed sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@
     If the number of arguments is 1, print 0
 */
 
-const numbers = process.argv.slice(2).map(a => parseInt(a, 10)).sort().reverse();
+const numbers = process.argv.slice(2).map(a => parseInt(a, 10)).sort((a, b) => b - a);
 
 if (!numbers.length || numbers.length === 1) {
   console.log(0);
