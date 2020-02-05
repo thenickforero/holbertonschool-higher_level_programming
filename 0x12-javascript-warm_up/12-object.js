1957437391867b3c5259ff8f7ccb38e6315dda90
NickForero11 <nicolasforero1199@gmail.com>
2020-02-05 02:25:02 -0500

    Added fixed script changing the value of a prop inside an object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// In the line 8 change the value of the myObject value to 89.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
