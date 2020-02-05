85459fbfe71484aac79ea09ece9851b25624c513
NickForero11 <nicolasforero1199@gmail.com>
2020-02-05 01:05:25 -0500

    Added a JS script to print strings from an array using a for of loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Script that prints 3 lines: (like 1-multi_languages.js)
+    but by using an array of string and a loop.
+*/
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}
