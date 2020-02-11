d5da20d977a19ff010bb91ddf705d3c0333b9daa
NickForero11 <nicolasforero1199@gmail.com>
2020-02-11 01:12:20 -0500

    Added a JS script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*  Script that imports a dictionary of occurrences by user id and
+    computes a dictionary of user ids by occurrence.
+
+    In the new dictionary:
+    - A key is a number of occurrences
+    - A value is the list of user ids
+
+*/
+const data = require('./101-data.js').dict;
+const ocurrences = {};
+for (const key in data) {
+  const idx = data[key];
+  if (ocurrences[idx] === undefined) {
+    ocurrences[idx] = [];
+  }
+  ocurrences[idx].push(key);
+}
+
+console.log(ocurrences);
