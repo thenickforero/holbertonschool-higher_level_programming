081feb397c1955dc12cafb9c7f2afb370e99af51
NickForero11 <nicolasforero1199@gmail.com>
2020-02-11 01:08:19 -0500

    Fixed relative import

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //  Script that imports an array and computes a new array using map.
 
-const data = require('./100-data').list;
+const data = require('./100-data.js').list;
 const parsedData = data.map((num, index) => num * index);
 console.log(data);
 console.log(parsedData);
