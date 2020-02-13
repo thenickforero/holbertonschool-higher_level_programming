1e71c45767727f3ec36e08d985f97bb4e9a8e259
NickForero11 <nicolasforero1199@gmail.com>
2020-02-12 19:04:38 -0500

    Improved style of script 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+/*  Script that prints all characters of a Star Wars movie:
+    - The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+    - Display one character name by line in the same order of the list “characters” in the /films/ response
+    - Must use the Star wars API
+    - Must use the module request
+*/
 
 const movieId = process.argv[2];
 const url = `http://swapi.co/api/films/${movieId}`;
@@ -6,7 +12,7 @@ const { get } = require('request');
 const { promisify } = require('util');
 const getCharacter = promisify(get);
 
-async function getCharacterName (url) {
+async function printCharacterName (url) {
   try {
     const character = await getCharacter(url);
     console.log(JSON.parse(character.body).name);
@@ -21,7 +27,7 @@ const filmHandler = async (error, response, body) => {
     }
     const characters = body.characters;
     for (const character of characters) {
-      await getCharacterName(character);
+      await printCharacterName(character);
     }
   }
 };
